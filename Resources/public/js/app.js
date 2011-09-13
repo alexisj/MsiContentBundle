a6ce1c2c49212abb27f0e85a424e6c05d179994b
Alexis Joubert <alexisjoubert@groupemsi.com>
2011-09-12 23:22:16 -0400

    design 3

--- Resources/public/js/app.js
+++
@@ -1,9 +0,0 @@
-// Dropdown for topbar nav
-// ===============================
-$("body").bind("click", function (e) {
-$('a.menu').parent("li").removeClass("open");
-});
-$("a.menu").click(function (e) {
-var $li = $(this).parent("li").toggleClass('open');
-return false;
-}); 
